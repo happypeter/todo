55059015b48c8722c33ff4c0f2ab7ff604c3cb1b
Peter Wang <happypeter1983@gmail.com>
2017-05-27 15:11:47 +0800

    COMPLETE todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import store from './redux/store'
 
 class TodoList extends React.Component {
+  handleClick(id) {
+    console.log(id)
+    store.dispatch({ type: 'COMPLETE', index: id - 1 })
+  }
+
   render() {
     let todoList = this.props.todos.map(item => (
-      <li key={Math.random()}>{item}</li>
+      <li onClick={this.handleClick.bind(this, item.id)}
+        className={item.completed ? 'completed' : ''}key={Math.random()}>
+        {item.content}
+      </li>
     ))
     return(
       <div className="todo-list">
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,17 @@
 let todos = [
-  "hello1",
-  "hello2",
-  "hello3"
+  { id: 1, content: "hello1", completed: false },
+  { id: 2, content: "hello2", completed: false }
 ]
 
 function rootReducer(state=todos, action) {
+  switch (action.type) {
+    case 'COMPLETE':
+      console.log(action.type, action.index)
+      const i = action.index
+      return [...state.slice(0,i), { ...state[i], completed: true }, ...state.slice(i + 1)]
+    default:
+      return state
+  }
   return state
 }
 
